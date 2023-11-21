63734038b94b1f15a7ec0c8612c9d070cf43675d
bateau moteur (brrrrrr) <107315159+asseukihuh@users.noreply.github.com>
2023-11-21 12:05:42 +0100

    Update nyharena.rakotovao.DS 2.py

--- 21.11.2023/nyharena.rakotovao.DS 2.py
+++ 21.11.2023/nyharena.rakotovao.DS 2.py
@@ -28,7 +28,29 @@ def dichotomie(tab, x):
         else:
             fin = m - 1
     return True
+
+#Recherche dichotomique par recurrence de https://github.com/anonymecrasher
+
+def recur_dichotomie(tab, x):
+    """
+    tab : tableau trie dans l'ordre croissant
+    x : nombre entier
+    La fonction renvoie True si tab contient x et False sinon
+    """
+    if len(tab) == 0:
+        return False,1
+    if (x < tab[0]) or (x > tab[len(tab)-1]):
+        return False, 2
     
+    m =  (len(tab)-1) // 2
+    if x == tab[m-1]:
+        return True
+    elif  x > tab[m]:
+        return recur_dichotomie(tab[m + 1:],x)
+    else:
+        return recur_dichotomie(tab[:m - 1],x)
+    
+    return False    
 
 #Programme principal
 
